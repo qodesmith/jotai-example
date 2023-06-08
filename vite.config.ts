ec5e69d9140427e6ff8ac0f75929bf83cb8d8395
Qodesmith <theqodesmith@gmail.com>
2023-06-08 18:33:44 -0400

    Adding Jotai plugins for Vite.

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,10 @@
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react'
+import jotaiDebugLabel from 'jotai/babel/plugin-debug-label'
+import jotaiReactRefresh from 'jotai/babel/plugin-react-refresh'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  // https://github.com/pmndrs/jotai/blob/main/docs/guides/vite.mdx
+  plugins: [react({babel: {plugins: [jotaiDebugLabel, jotaiReactRefresh]}})],
 })
