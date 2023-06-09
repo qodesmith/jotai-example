980927644a32ab66b88cecef03845a54125b7569
Qodesmith <theqodesmith@gmail.com>
2023-06-08 20:24:50 -0400

    Added comment about tying.

--- src/SuspenseValue.tsx
+++ src/SuspenseValue.tsx
@@ -1,4 +1,4 @@
-import {atom, useAtomValue} from 'jotai'
+import {atom, useAtomValue, ExtractAtomValue} from 'jotai'
 
 export default function SuspenseValue() {
   const promiseSelectorValue = useAtomValue(promiseSelector)
@@ -11,9 +11,14 @@ const privateAtom = atom('hello world')
 
 /**
  * Jotai will suspend since this selector returns a promise.
+ *
+ * We can use Jotai's type helper - ExtractAtomValue - to infer the type from
+ * the atom we consume in the selector.
  */
-const promiseSelector = atom<Promise<string>>(get => {
-  return new Promise(resolve => {
-    setTimeout(() => resolve(get(privateAtom)), 2000)
-  })
-})
+const promiseSelector = atom<Promise<ExtractAtomValue<typeof privateAtom>>>(
+  get => {
+    return new Promise(resolve => {
+      setTimeout(() => resolve(get(privateAtom)), 2000)
+    })
+  }
+)
